6896a4eb676b8197db9f973654ae91faf3ee23cb
alpoaa <aleksi.e.aalto@gmail.com>
2023-12-22 16:56:46 +0200

    puhelinluettelon backend, step9

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const morgan  = require('morgan')
 const app     = express()
+const cors    = require('cors')
 
 app.use(express.json())
+app.use(cors())
 
 morgan.token('body', (request) => JSON.stringify(request.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
@@ -100,7 +102,7 @@ app.delete('/api/persons/:id', (request, response) => {
     }
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
